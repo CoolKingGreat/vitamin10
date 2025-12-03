7b658be468a2cb1f22bbedf9f88ff2d57d7beaf7
Aryan Valsa Pradeep <aryan@wifi-10-42-55-199.wifi.berkeley.edu>
2025-12-03 03:57:20 -0800

    added render url to frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const fetchQuote = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/quote');
+      const response = await fetch('https://vitamin10-vq97.onrender.com/api/quote');
       const data = await response.json();
       setQuote(data.quote);
     } catch (error) {
